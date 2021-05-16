c9f3c0f790fc503c618ecff422218cea1355808d
BOXXQAHDX <novikova988247@gmail.com>
2021-05-16 19:38:31 +0700

    v0.0.0.2

--- code/zag.py
+++ code/zag.py
@@ -1,7 +1,17 @@
 #программа установщик
-#v0.0.1
-def menu ():
+#v0.0.0.2
+
+import os 
+
+def list_of_programs():
+	os.chdir(r"..\bashs")
+	x = os.listdir()
+	print (x)
+
 	
+
+def menu ():
+	'''основная функция программы'''
 	print ('----ZAG----------------------------------')
 	print ("[1] - список программ")
 	print ("[2] - найти на устройстве")
@@ -11,3 +21,26 @@ def menu ():
 	print ("[6] - установленые программы")
 	print ("[0] - выйти")
 	inp = input (": ")
+
+	if inp == "1":
+		list_of_programs ()
+
+	elif inp == "2":
+		pass
+
+	elif inp == "2":
+		pass
+
+	elif inp == "2":
+		pass
+
+	elif inp == "2":
+		pass
+
+	elif inp == "2":
+		pass
+
+	else:
+		pass
+
+menu ()
